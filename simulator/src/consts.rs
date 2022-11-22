22bde9524540bf6b1311a921f612e55aa23356d3
Eise Zimmerman <eisezimmerman@localhost.localdomain>
2022-11-22 10:18:47 +0100

    Simulation: Set repulsion factor

--- simulator/src/consts.rs
+++ simulator/src/consts.rs
@@ -9,10 +9,7 @@ pub const TIMESTEP: f32 = 0.01;
 // Constants for formula's
 pub const GRAVITY_ACC_VECTOR: Vector<f32> = Vector { x: 0., y: -9.81, z: 0. };
 pub const DAMPING_CONSTANT: f32 = 1.; // TODO: What is good value for this?
-pub const ATTRACTION_EXPONENT: i32 = 6;
-pub const REPULSION_EXPONENT: i32 = 12;
-pub const ATTRACTION_FACTOR: f32 = 0.000000000000000002737205; // TODO: What is good value for this?
-pub const REPULSION_FACTOR: f32 = 377616490000000000000.; // TODO: What is good value for this?
+pub const REPULSION_FACTOR: f32 = 1.;
                                                       //
 // Particle properties
 pub const PARTICLE_MASS: f32 = 0.01; // TODO: Calculate based on dimentions
